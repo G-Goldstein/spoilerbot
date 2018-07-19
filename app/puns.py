6be3a59754625bd844db0ae6993ea7698b2e438b
Graham Goldstein <graham.goldstein@jhc.co.uk>
2018-07-19 09:42:50 +0100

    Fix missing image

--- app/puns.py
+++ app/puns.py
@@ -3,7 +3,7 @@ import random
 choices = [
 ('Awaken the Scry Tyrant', 'https://www.fgrecservices.com/wp-content/uploads/2015/11/WS108369.jpg'),
 ('Call the Scrybreaker', 'https://s3-us-west-2.amazonaws.com/echomage/cards/CMD/247202.crop.hq.jpg'),
-('Daring Scryjek', 'http://www.artofmtg.com/wp-content/uploads/2014/01/Daring-Skyjek-Art.jpg'),
+('Daring Scryjek', 'https://i.pinimg.com/736x/e6/9f/e4/e69fe4a454f4cf97872fec0bc58477a2--mtg-art-character-art.jpg'),
 ('Eldrazi Scryspawner', 'https://pre00.deviantart.net/2752/th/pre/i/2015/363/9/2/eldrazi_skyspawner_by_chasestone-d9lxx91.jpg'),
 ("Isperia's Scrywatch", 'http://1.bp.blogspot.com/-9X3uUpJR38c/UG80gSsqrtI/AAAAAAAAA0Q/aQXgqfVoqHY/s1600/Isperia%27s+Skywatch.png'),
 ('Kor Scryfisher', 'http://magic.wizards.com/sites/mtg/files/image_legacy_migration/mtg/images/daily/arcana/332_final.jpg'),
