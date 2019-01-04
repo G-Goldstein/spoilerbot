787e51e0b25a07ef90a638f2ec91d5b8b9bfd892
Graham Goldstein <graham.goldstein@jhc.co.uk>
2019-01-04 16:15:49 +0000

    Handle cards with multiple faces

--- app/scryfall_to_slack.py
+++ app/scryfall_to_slack.py
@@ -2,21 +2,35 @@ import re
 
 def make_attachment_from_card(card):
   attachment = {}
-  attachment['title'] = card['name'] + ' ' + manaify(card['mana_cost'])
+  card_faces = list(faces(card))
+  attachment['title'] = card_faces[0]['name'] + ' ' + manaify(card_faces[0]['mana_cost'])
   attachment['title_link'] = card['scryfall_uri']
   attachment['thumb_url'] = card['image_uris']['small']
-  lines = [card['type_line'], manaify(card['oracle_text'])]
-  if 'flavor_text' in card:
-    lines.append('\n'.join(map(lambda s: '_' + s + '_', card['flavor_text'].split('\n'))))
-  if 'power' in card and 'toughness' in card:
-    lines.append('*' + card['power'] + '/' + card['toughness'] + '*')
-  if 'loyalty' in card:
-    lines.append('*Loyalty: ' + card['loyalty'] + '*')
-  attachment['text'] = '\n'.join(lines)
+  lines = []
+  for face in card_faces:
+    face_lines = []
+    if lines:
+      face_lines.append('*' + face['name'] + '* ' + manaify(face['mana_cost']))
+      face_lines.append(face['type_line'])
+    face_lines.append(manaify(face['oracle_text']))
+    if 'flavor_text' in face:
+      face_lines.append('\n'.join(map(lambda s: '_' + s + '_', face['flavor_text'].split('\n'))))
+    if 'power' in face and 'toughness' in card:
+      face_lines.append('*' + face['power'] + '/' + face['toughness'] + '*')
+    if 'loyalty' in face:
+      face_lines.append('*Loyalty: ' + face['loyalty'] + '*')
+    lines.append('\n'.join(face_lines))
+  attachment['text'] = '\n---------\n'.join(lines)
   attachment['mrkdwn_in'] = ['text']
-  attachment['footer'] = card['rarity'].capitalize() + '  -  ' + card['set_name'] + ' #' + card['collector_number']
+  attachment['footer'] = card['set_name'] + ' #' + card['collector_number'] + '  -  ' + card['rarity'].capitalize()
   # attachment['color'] = frame_color(card['colors']) This doesn't look good.
   return attachment
+
+def faces(card):
+  if 'card_faces' in card:
+    yield from card['card_faces']
+  else:
+    yield card
 
 def manaify(string):
   without_mana_symbol_slashes = re.sub(r'({\w)/(\w})',
