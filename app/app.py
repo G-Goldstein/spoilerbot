df69e8d2d61bb7ba1eff80b44a37a3b76b6c5577
Graham Goldstein <graham.goldstein@fnz.com>
2020-12-22 15:38:20 +0000

    Add more debug logging

--- app/app.py
+++ app/app.py
@@ -14,11 +14,14 @@ def spoil_cards(set, cards):
 def spoil_new_cards():
   upcoming_sets = scryfall.upcoming_sets()
   if not upcoming_sets:
+    print('No new sets; sleeping for 4 hours')
     time.sleep(60*60*4) # No new sets, check again in 4 hours
   for set in upcoming_sets:
     set_code = set['code']
+    print('Upcoming set: {}'.format(set_code))
     new_spoilers = [card for card in scryfall.spoiled_cards(set_code) if card['name'] not in spoiled.previously_spoiled_names(set_code) and card['name'] not in never_post_card_names][:5]
     if not new_spoilers:
+      print('No new spoilers in {}'.format(set_code))
       continue
     try:
       spoil_cards(set, new_spoilers)
